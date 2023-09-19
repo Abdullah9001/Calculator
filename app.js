b22fd64ef6de102c0d0a79c2554263838693b821
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-09-19 17:10:47 +0600

    done canculator project

---
+++ app.js
@@ -0,0 +1,13 @@
+function num(val) {
+  document.getElementById("result").value += val;
+}
+
+function equal() {
+  let Input = document.getElementById("result").value;
+  let Output = eval(Input);
+  document.getElementById("result").value = Output;
+}
+
+function clr() {
+  document.getElementById("result").value = "";
+}
